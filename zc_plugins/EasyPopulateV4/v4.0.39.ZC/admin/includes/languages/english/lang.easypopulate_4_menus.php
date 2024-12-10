d51613a6a406806d1c5e214b042bb2bb8914ae92
OldNGrey <73047349+OldNGreyBMR@users.noreply.github.com>
2024-12-11 08:52:28 +1000

    Add files via upload

---
+++ zc_plugins/EasyPopulateV4/v4.0.39.ZC/admin/includes/languages/english/lang.easypopulate_4_menus.php
@@ -0,0 +1,32 @@
+<?php
+// $Id: easypopulate_4.php, v4.0.37 02-08-2017 mc12345678 $
+
+// $display_output defines
+
+// file uploads display - output via $display_output
+$define = [
+    'EASYPOPULATE_4_DISPLAY_MANUFACTURERS' => 'Manufacturers',
+    'EASYPOPULATE_4_DISPLAY_MANUFACTURERS_NONE' => 'No Manufacturers',
+    'EASYPOPULATE_4_DISPLAY_FILTERABLE_EXPORTS' => 'Filterable Exports:',
+    'EASYPOPULATE_4_DISPLAY_PRODUCTS_PRICE_EXPORT_OPTION' => 'Product &amp; Pricing Export/Import Options:',
+    'EASYPOPULATE_4_DISPLAY_COMPLETE_PRODUCTS' => '<b>Complete Products</b> (with Metatags)',
+    'EASYPOPULATE_4_DISPLAY_COMPLETE_PRODUCTS_SINGLE' => '<b>Complete Products By master_categories_id (without Linked)</b> (with Metatags)',
+    'EASYPOPULATE_4_DISPLAY_PRICE_QTY' => '<b>Model/Price/Qty</b> (with Specials)',
+    'EASYPOPULATE_4_DISPLAY_PRICE_BREAKS' => '<b>Model/Price/Breaks</b>',
+    'EASYPOPULATE_4_DISPLAY_FEATURED' => '<b>Featured Products</b>',
+    'EASYPOPULATE_4_DISPLAY_TITLE_CATEGORY','Category Export/Import Options',
+    'EASYPOPULATE_4_DISPLAY_EXPORT_CATEGORY','<b>Model/Category</b>',
+    'EASYPOPULATE_4_DISPLAY_EXPORT_CATEGORYMETA','<b>Categories Only</b> (with Metatags)',
+    'EASYPOPULATE_4_DISPLAY_TITLE_ATTRIBUTE','<b>Attribute Export/Import Options</b>',
+    'EASYPOPULATE_4_DISPLAY_EXPORT_ATTRIBUTE_BASIC','<b>Basic Products Attributes</b> (basic single-line)',
+    'EASYPOPULATE_4_DISPLAY_EXPORT_ATTRIBUTE_DETAILED','<b>Detailed Products Attributes</b> (detailed multi-line)',
+    'EASYPOPULATE_4_DISPLAY_EXPORT_DETAILED_SBA' => '<b>Detailed Stock By Attributes Data</b> (detailed multi-line)',
+    'EASYPOPULATE_4_DISPLAY_EXPORT_SBA_STOCK' => '<b>Stock of Items with Attributes Including SBA</b>',
+    'EASYPOPULATE_4_DISPLAY_EXPORT_SBA_STOCK_ASC' => '<b>Stock of Items with Attributes Including SBA Sorted Ascending</b>',
+    'EASYPOPULATE_4_DISPLAY_TITLE_EXPORT_ONLY' => 'DIAGNOSTIC EXPORTS - Note: NOT FOR IMPORTING ATTRIBUTES!',
+    'EASYPOPULATE_4_DISPLAY_EXPORT_OPTION_NAMES' => '<b>Attribute Options Names</b>',
+    'EASYPOPULATE_4_DISPLAY_EXPORT_OPTION_VALUES' => '<b>Attribute Options Values</b>',
+    'EASYPOPULATE_4_DISPLAY_EXPORT_OPTION_NAMES_TO_VALUES' => '<b>Attribute Options-Names-to-Values</b>',
+
+];
+return $define;

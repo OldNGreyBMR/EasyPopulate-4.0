55b991622ce13abb6e60abeebded60adeb78ade4
OldNGrey <73047349+OldNGreyBMR@users.noreply.github.com>
2024-12-11 11:50:35 +1000

    Update manifest.php
    pluginAuthor max size 64 char

--- zc_plugins/EasyPopulateV4/v4.0.39.ZC/manifest.php
+++ zc_plugins/EasyPopulateV4/v4.0.39.ZC/manifest.php
@@ -3,7 +3,7 @@ return [
     'pluginVersion' => 'v4.0.39.ZC',
     'pluginName' => 'EasyPopulate V4 (EP4)',
     'pluginDescription' => 'Means to access database information for product in a mass method rather than manipulating product information one screen at a time.',
-    'pluginAuthor' => 'mc12345678 (McNumbers Ware, mc12345678.com) with ChadD as original author',
+    'pluginAuthor' => 'mc12345678 (McNumbers Ware, mc12345678.com) with ChadD as orig author',
     'pluginId' => 2069, // ID from Zen Cart forum
     'zcVersions' => ['v139','v150','v151','v153','v154','v155','v156','v157','v158','v200','v201'],
     'changelog' => 'changelog.txt', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
